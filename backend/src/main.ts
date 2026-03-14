afb6a2b1c9074fe944a7aede764bfebabcbd91e2
Joshua Vincent Padilla <joshuapadilla706@gmail.com>
2026-03-14 15:40:19 +0800

    fix: update global API prefix to standard '/api'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
-  app.setGlobalPrefix('syncfit/api');
+  app.setGlobalPrefix('/api');
   app.enableCors({
     origin: [
       'http://localhost:3000',
